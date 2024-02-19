59fa11afe21a781cef37ae2d11cfc93e2a8dbd40
ruchiii-codes <ruchi06052005@gmail.com>
2024-02-19 14:44:02 +0530

    If Else Program

---
+++ If_else.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+
+    int A;
+    cin>>A;
+
+    if(A>0)
+      {
+        cout<<"A is Positive";
+      }
+    else
+      {
+        cout<<"A is Negative";
+      }  
+      
+}
