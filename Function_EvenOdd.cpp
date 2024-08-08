96d2ed8cdade734084b0d1e4d4427824a68d032a
Ruchika Vispute <125819611+ruchiii-codes@users.noreply.github.com>
2024-08-08 22:35:08 +0530

    Function_EvenOdd.cpp

--- Function_EvenOdd.cpp
+++ Function_EvenOdd.cpp
@@ -5,14 +5,13 @@ bool isEven(int a)
 {
    if(a&1)
    return 0;
-
    return 1; 
 }
 int main ()
 {
    int num;
    cin >> num;
-
+   
    if(isEven(num))
    {
       cout<<"Number is Even"<<endl;
